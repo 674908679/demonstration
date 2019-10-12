fa8f2e525b3b389d73e3b06a791e240e95de9242
674908679 <674908679@qq.com>
2019-10-12 20:10:12 +0800

    second

--- src/helloword.java
+++ src/helloword.java
@@ -2,5 +2,6 @@ public class helloword {
     public static void main(String[] args){
         System.out.println("how are you");
         System.out.println("i'm fine");
+        System.out.println("thanks,and you?");
     }
 }
